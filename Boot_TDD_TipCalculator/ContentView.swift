436856569732e28f746e341a86e3578bd7c389b1
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-09-23 22:27:53 +0200

    Basic UI done

--- Boot_TDD_TipCalculator/ContentView.swift
+++ Boot_TDD_TipCalculator/ContentView.swift
@@ -3,14 +3,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var total: String = ""
+    @State private var tipPercent: Double = 0.2
+    @State private var tip: String?
+    @State private var message: String = "asdf"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                TextField("Enter total", text: $total)
+                    .textFieldStyle(.roundedBorder)
+                
+                Picker(selection: $tipPercent) {
+                    Text("10%").tag(0.1)
+                    Text("20%").tag(0.2)
+                    Text("30%").tag(0.3)
+                } label: {
+                    EmptyView()
+                }
+                .pickerStyle(.segmented)
+                .padding(.top)
+
+                Button("Calculate Tip") {
+                    
+                }
+                .padding(.top, 32)
+                .buttonStyle(.borderedProminent)
+              
+                Text(message)
+                    .padding(.top, 64)
+                
+                Spacer()
+                Text(tip ?? "")
+                    .font(.system(size: 54))
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Tip Calculator")
         }
-        .padding()
     }
 }
 
