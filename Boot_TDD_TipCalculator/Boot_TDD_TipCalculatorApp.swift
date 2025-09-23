eab4f544d79303cafb7c881ee7e86203b2f16da8
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-09-23 21:43:21 +0200

    Initial Commit

---
+++ Boot_TDD_TipCalculator/Boot_TDD_TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boot_TDD_TipCalculatorApp.swift
+//  Boot_TDD_TipCalculator
+//
+//  Created by Miguel Gallego on 23/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Boot_TDD_TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boot_TDD_TipCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Boot_TDD_TipCalculator
+//
+//  Created by Miguel Gallego on 23/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
