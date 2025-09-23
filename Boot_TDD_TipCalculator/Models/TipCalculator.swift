02a0f284ef06035310ef53616b8099e5920a62f9
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-09-23 23:13:49 +0200

    test_should_calculate_tip_succesfully() passed

--- Boot_TDD_TipCalculator/Models/TipCalculator.swift
+++ Boot_TDD_TipCalculator/Models/TipCalculator.swift
@@ -1,3 +1,10 @@
 //  TipCalculator.swift
 //  Boot_TDD_TipCalculator
 import Foundation
+
+class TipCalculator {
+    
+    func calculate(total: Double, tipPercent: Double) -> Double {
+        return total * tipPercent
+    }
+}
