7606a13846d86ef8190eaf2938077a916c7e5dd2
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-09-23 23:30:53 +0200

    WIP doing a new test

--- Boot_TDD_TipCalculatorUnitTests/Boot_TDD_TipCalculatorUnitTests.swift
+++ Boot_TDD_TipCalculatorUnitTests/Boot_TDD_TipCalculatorUnitTests.swift
@@ -6,10 +6,18 @@ import XCTest
 final class When_calculating_tip_based_on_total_amount: XCTestCase {
 
     func test_should_calculate_tip_succesfully() {
-        
         let tipCalculator = TipCalculator()
         let tip = tipCalculator.calculate(total: 100, tipPercent: 0.1)
         XCTAssertEqual(10, tip)
     }
 
+}
+
+// A new Case, a new context
+final class When_calculating_tip_based_on_negative_total_amout: XCTestCase {
+    
+    func test_should_throw_invalid_input_exception() {
+        
+    }
+    
 }
