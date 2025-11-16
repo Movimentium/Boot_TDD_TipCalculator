9171981498889199f0042e9724ff39479c4f858e
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-11-16 02:02:42 +0100

    UI Tests refactor

--- Boot_TDD_TipCalculatorUITests/Boot_TDD_TipCalculatorUITests.swift
+++ Boot_TDD_TipCalculatorUITests/Boot_TDD_TipCalculatorUITests.swift
@@ -4,10 +4,15 @@ import XCTest
 
 class when_content_view_is_shown: XCTestCase {
 
-    func test_total_text_field_contains_default_value() {
-        let app = XCUIApplication()
+    private var app: XCUIApplication!
+    
+    override func setUp() {
+        app = XCUIApplication()
         continueAfterFailure = false
         app.launch()
+    }
+    
+    func test_total_text_field_contains_default_value() {
         
         let totalTextField = app.textFields["totalTextField"]
         
@@ -15,9 +20,6 @@ class when_content_view_is_shown: XCTestCase {
     }
     
     func test_the_20_percent_default_tip_is_selected() {
-        let app = XCUIApplication()
-        continueAfterFailure = false
-        app.launch()
         
         let tipPicker = app.segmentedControls["tipPicker"]
         let pickerButton = tipPicker.buttons.element(boundBy: 1)
